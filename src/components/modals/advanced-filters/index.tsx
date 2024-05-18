f29648d3949bf1eff060d56d7004b5d38e076d64
Ziad-Helal <zgalhelaly@gmail.com>
2024-05-18 08:11:24 +0300

    merge

--- src/components/modals/advanced-filters/index.tsx
+++ src/components/modals/advanced-filters/index.tsx
@@ -18,6 +18,7 @@ export function AdvancedFilters_Modal() {
           size="icon"
           className="rounded-full p-2"
           title="Search Filters"
+          disabled
         >
           <SlidersHorizontal />
         </Button>
